6e2dd423341a6b6f14675bced3ba197312d20dc6
Arthur Weinberger <arthurweinberger@gmail.com>
2021-03-14 19:00:19 -0700

    Working preliminary parser for basic arithmetic.

---
+++ interview.js
@@ -0,0 +1,315 @@
+function ParseError(msg, token, model) {
+  var idx = token[1];
+  var text = model.text;
+  throw "ERROR: " + msg +
+        "\nidx = " + idx +
+        "\n" + text.substr(0, idx + 1) + "/* Here */" +
+        text.substr(idx + 1);
+}
+
+// Other types of tokens have exactly 1 character
+var WHITE_SPACE_TOKEN = "ws";
+var IDENTIFIER_TOKEN = "id";
+var COMMENT_TOKEN = "cmt";
+var DIGITS_TOKEN = "dgt"
+var STRING_TOKEN = "str"
+var NUMBER_TOKEN = "num"
+
+function Tokenize(model) {
+  var WHITE_SPACE = /\s/;
+  var COMMENT_START = "/*";
+  var COMMENT_END = "*/";
+  var STRING_DELIM = "\"";
+  var STRING_ESCAPE = "\\";
+  var DIGIT = /[0-9]/;
+  var DIGIT_OR_DOT = /[.0-9]/;
+  var IDENTIFIER_1 = /[a-zA-Z_]/;
+  var IDENTIFIER_2 = /[a-zA-Z_0-9]/;
+
+  var tokens = [];  // list of tokens [type, start_idx, len]
+  var idx = 0;
+  var text = model.text;
+  while (idx < text.length) {
+    if (WHITE_SPACE.test(text[idx])) {
+      var token = [];
+      token[0] = WHITE_SPACE_TOKEN;
+      token[1] = idx;
+      while (idx < text.length && WHITE_SPACE.test(text[idx])) {
+        ++idx;
+      }
+      token[2] = idx - token[1];
+      tokens.push(token);
+      continue;
+    }
+    if (IDENTIFIER_1.test(text[idx])) {
+      var token = [];
+      token[0] = IDENTIFIER_TOKEN;
+      token[1] = idx;
+      while (idx < text.length && IDENTIFIER_2.test(text[idx])) {
+        ++idx;
+      }
+      token[2] = idx - token[1];
+      tokens.push(token);
+      continue;
+    }
+    if (COMMENT_START == text.substr(idx, 2)) {
+      var token = [];
+      token[0] = COMMENT_TOKEN;
+      token[1] = idx;
+      while (idx < text.length && COMMENT_END != text.substr(idx, 2)) {
+        ++idx;
+      }
+      if (COMMENT_END == text.substr(idx, 2)) {
+        idx += 2;
+      }
+      token[2] = idx - token[1];
+      tokens.push(token);
+      continue;
+    }
+    if (DIGIT.test(text[idx])) {
+      var token = [];
+      token[0] = NUMBER_TOKEN;
+      token[1] = idx;
+      var num_dots = 0;
+      while (idx < text.length && DIGIT_OR_DOT.test(text[idx])) {
+        if (text[idx] == ".") {
+          ++num_dots;
+          if (num_dots >= 2) {
+            var token = [];
+            token[1] = idx;
+            ParseError("Unexpected second dot in number.", token, model);
+          }
+        }
+        ++idx;
+      }
+      token[2] = idx - token[1];
+      tokens.push(token);
+      continue;
+    }
+    if (STRING_DELIM == text[idx]) {
+      var token = [];
+      token[0] = STRING_TOKEN;
+      token[1] = idx;
+      ++idx;
+      // TODO: Handle STRING_ESCAPE
+      while (idx < text.length && STRING_DELIM != text[idx]) {
+        ++idx;
+      }
+      if (STRING_DELIM == text[idx]) {
+        ++idx;
+      }
+      token[2] = idx - token[1];
+      tokens.push(token);
+      continue;
+    }
+    {
+      // All other tokens
+      var token = [];
+      token[0] = text[idx];
+      token[1] = idx;
+      token[2] = 1;
+      tokens.push(token);
+      ++idx;
+    }
+  }
+  model.tokens = tokens;
+}
+function CleanTokens(tokens, text) {
+  // Drop whitespace and comments.
+  var clean_tokens = [];
+  var idx = 0;
+  while (idx < tokens.length) {
+    if (tokens[idx][0] == WHITE_SPACE_TOKEN) {
+      ++idx;
+      continue;
+    }
+    if (tokens[idx][0] == COMMENT_TOKEN) {
+      ++idx;
+      continue;
+    }
+    clean_tokens.push(tokens[idx]);
+    ++idx;
+  }
+  return clean_tokens;
+}
+function TokenType(model, idx) {
+  return model.tokens[idx][0];
+}
+function TokenString(model, idx) {
+  return model.text.substr(model.tokens[idx][1],
+                           model.tokens[idx][2]);
+}
+function TokenToNode(model) {
+  var node = {};
+  node.type = TokenType(model, model.token_idx);
+  node.token = model.tokens[model.token_idx];
+  node.parent = null;
+  node.left = null;
+  node.right = null;
+  model.token_idx += 1;
+  if (node.type == NUMBER_TOKEN) {
+    node.right = Number(TokenString(model, model.token_idx - 1));
+  }
+  if (node.type == STRING_TOKEN) {
+    node.right = String(TokenString(model, model.token_idx - 1));
+  }
+  return node;
+}
+function AppendBelowRight(node, new_node) {
+  new_node.parent = node;
+  new_node.left = node.right;
+  node.right = new_node;
+  if (new_node.left != null) {
+    new_node.left.parent = new_node;
+  }
+}
+function Remove(node) {
+  // TODO: Do error checking...
+  // This should only work if the node has at most one child.
+  // For now we assume it has a right child if any.
+  if (node.parent.right == node) {
+    node.parent.right = node.right;
+    if (node.right != null) {
+      node.right.parent = node.parent;
+    }
+  }
+}
+function HandleNewNode(model, curr, new_node) {
+  if (["+", "-", "*", "/", "head", "("].includes(curr.type)) {
+    if ([NUMBER_TOKEN, "(", STRING_TOKEN].includes(new_node.type)) {
+      AppendBelowRight(curr, new_node);
+      return new_node;
+    }
+  }
+  if ([NUMBER_TOKEN, "()",].includes(curr.type)) {
+    if (["*", "/"].includes(new_node.type)) {
+      while (["*", "/", NUMBER_TOKEN, "()"].includes(curr.type)) {
+        curr = curr.parent;
+      }
+      AppendBelowRight(curr, new_node);
+      return new_node;
+    }
+    if (["+", "-", ")"].includes(new_node.type)) {
+      while (["+", "-", "*", "/", NUMBER_TOKEN, "()"].includes(curr.type)) {
+        curr = curr.parent;
+      }
+      if (curr.type == "(" && new_node.type == ")") {
+        curr.type = "()";
+        return curr;
+      }
+      AppendBelowRight(curr, new_node);
+      return new_node;
+    }
+  }
+  ParseError("Can't add " + new_node.type + " after " + curr.type,
+             new_node.token, model);
+  return new_node;
+}
+function ParseExpression(model) {
+  var head = {};
+  head.type = "head";
+  var curr = head;
+  while (model.token_idx < model.tokens.length &&
+         TokenType(model, model.token_idx) != ";") {
+    var new_node = TokenToNode(model);
+    curr = HandleNewNode(model, curr, new_node);
+  }
+  return head.right;
+}
+function ExpressionDebugString(model, expr) {
+  if (expr.type == NUMBER_TOKEN) {
+    return String(expr.right);
+  }
+  if (expr.type == STRING_TOKEN) {
+    return String(expr.right);
+  }
+  if (expr.type == "()") {
+    return ExpressionDebugString(model, expr.right);
+  }
+  if (expr.type == "+") {
+    return ExpressionDebugString(model, expr.left) + " " +
+           ExpressionDebugString(model, expr.right) + " +";
+  }
+  if (expr.type == "-") {
+    return ExpressionDebugString(model, expr.left) + " " +
+           ExpressionDebugString(model, expr.right) + " -";
+  }
+  if (expr.type == "*") {
+    return ExpressionDebugString(model, expr.left) + " " +
+           ExpressionDebugString(model, expr.right) + " *";
+  }
+  if (expr.type == "/") {
+    return ExpressionDebugString(model, expr.left) + " " +
+           ExpressionDebugString(model, expr.right) + " /";
+  }
+  return "Unexpected expression type: " + expr.type;
+}
+function Evaluate(model, expr) {
+  if (expr.type == NUMBER_TOKEN) {
+    return Number(expr.right);
+  }
+  if (expr.type == STRING_TOKEN) {
+    return String(expr.right);
+  }
+  if (expr.type == "+") {
+    return Evaluate(model, expr.left) + Evaluate(model, expr.right);
+  }
+  if (expr.type == "-") {
+    return Evaluate(model, expr.left) - Evaluate(model, expr.right);
+  }
+  if (expr.type == "*") {
+    return Evaluate(model, expr.left) * Evaluate(model, expr.right);
+  }
+  if (expr.type == "/") {
+    return Evaluate(model, expr.left) / Evaluate(model, expr.right);
+  }
+  if (expr.type == "()") {
+    return Evaluate(model, expr.right);
+  }
+  // TODO: error message!
+  return 777;
+}
+function ParseStatement(model) {
+  var idx = model.token_idx;
+  var first_token_idx = idx;
+  if (model.tokens[idx][0] != IDENTIFIER_TOKEN) {
+    ParseError("Expected Identifier.",
+               model.tokens[first_token_idx],
+               model);
+  }
+  var name = model.text.substr(model.tokens[idx][1], model.tokens[idx][2]);
+  ++idx;
+  if (idx >= model.tokens.length || model.tokens[idx][0] != "=") {
+    ParseError("Expected '='.",
+               model.tokens[idx],
+               model);
+  }
+  ++idx;
+  model.token_idx = idx;
+  var expression = ParseExpression(model);
+  var last_token_idx = model.token_idx;
+  model.statement_list.push(
+      {
+        first_token_idx: first_token_idx,
+        last_token_idx: last_token_idx,
+        name: name,
+        expression: expression,
+        value: Evaluate(model, expression),
+      });
+  model.token_idx = last_token_idx + 1;
+}
+function Parse(text) {
+  var model = {};
+  model.text = text;
+  Tokenize(model);
+  model.statement_list = [];
+  model.token_idx = 0;
+  // TODO: Don't pass out the tokens from Tokenize. Instead pass in a model.
+  // Then update errors in the model.
+  model.tokens = CleanTokens(model.tokens, text);
+  while (model.token_idx < model.tokens.length) {
+    ParseStatement(model);
+  }
+  return model;
+}
+
---
+++ interview_test.js
@@ -0,0 +1,383 @@
+function FAIL(msg) {
+  console.trace("ERROR: " + msg);
+}
+function EXPECT_EQ(a, b) {
+  if (a==b) return;
+  console.trace("ERROR: Not Equal: [", a, "] = [", b, "]");
+}
+function EXPECT_INCLUDES(list, value) {
+  if (list.includes(value)) return;
+  console.trace("ERROR: Does not include [", value, "]: [", list, "]");
+}
+
+function TokenizeForTest(str) {
+  var model = {};
+  model.text = str;
+  Tokenize(model);
+  return model.tokens;
+}
+function TestTokenizeNumbers() {
+  var model = {};
+  model.text = "1020339";
+  Tokenize(model);
+  var tokens = model.tokens;
+  EXPECT_EQ(tokens.length, 1);
+  EXPECT_EQ(tokens[0][0], NUMBER_TOKEN);
+  EXPECT_EQ(tokens[0][1], 0);
+  EXPECT_EQ(tokens[0][2], 7);
+
+  model = {};
+  model.text = "1020.339";
+  Tokenize(model);
+  tokens = model.tokens;
+  EXPECT_EQ(tokens.length, 1);
+  EXPECT_EQ(tokens[0][0], NUMBER_TOKEN);
+  EXPECT_EQ(tokens[0][1], 0);
+  EXPECT_EQ(tokens[0][2], 8);
+
+  try {
+    model = {};
+    model.text = "1020.33.9";
+    Tokenize(model);
+    tokens = model.tokens;
+    FAIL("Should throw an error because of extra  dot");
+  } catch(err) {
+    EXPECT_EQ(
+        err,
+        "ERROR: Unexpected second dot in number.\nidx = 7\n" +
+        "1020.33./* Here */9");
+  }
+
+  model = {};
+  model.text = "1020.33a";
+  Tokenize(model);
+  tokens = model.tokens;
+  EXPECT_EQ(tokens.length, 2);
+  EXPECT_EQ(tokens[0][0], NUMBER_TOKEN);
+  EXPECT_EQ(tokens[0][1], 0);
+  EXPECT_EQ(tokens[0][2], 7);
+  EXPECT_EQ(tokens[1][0], IDENTIFIER_TOKEN);
+  EXPECT_EQ(tokens[1][1], 7);
+  EXPECT_EQ(tokens[1][2], 1);
+
+  tokens = TokenizeForTest("a990.33");
+  EXPECT_EQ(tokens.length, 3);
+  EXPECT_EQ(tokens[0][0], IDENTIFIER_TOKEN);
+  EXPECT_EQ(tokens[0][1], 0);
+  EXPECT_EQ(tokens[0][2], 4);
+  EXPECT_EQ(tokens[1][0], ".");
+  EXPECT_EQ(tokens[1][1], 4);
+  EXPECT_EQ(tokens[1][2], 1);
+  EXPECT_EQ(tokens[2][0], NUMBER_TOKEN);
+  EXPECT_EQ(tokens[2][1], 5);
+  EXPECT_EQ(tokens[2][2], 2);
+}
+function TestTokenizeOneToken() {
+  var tokens = TokenizeForTest(" \n\r\t ");
+  EXPECT_EQ(tokens.length, 1);
+  EXPECT_EQ(tokens[0][0], WHITE_SPACE_TOKEN);
+  EXPECT_EQ(tokens[0][1], 0);
+  EXPECT_EQ(tokens[0][2], 5);
+
+  tokens = TokenizeForTest("aabb09A_sdf");
+  EXPECT_EQ(tokens.length, 1);
+  EXPECT_EQ(tokens[0][0], IDENTIFIER_TOKEN);
+  EXPECT_EQ(tokens[0][1], 0);
+  EXPECT_EQ(tokens[0][2], 11);
+
+  tokens = TokenizeForTest("/* Hello world 123 \" *&^  /* haha */");
+  EXPECT_EQ(tokens.length, 1);
+  EXPECT_EQ(tokens[0][0], COMMENT_TOKEN);
+  EXPECT_EQ(tokens[0][1], 0);
+  EXPECT_EQ(tokens[0][2], 36);
+  EXPECT_EQ("/* Hello world 123 \" *&^  /* haha */".length, 36);
+
+  var s = "\"Hello world /* blah blah */ 23123\"";
+  tokens = TokenizeForTest(s);
+  EXPECT_EQ(tokens.length, 1);
+  EXPECT_EQ(tokens[0][0], STRING_TOKEN);
+  EXPECT_EQ(tokens[0][1], 0);
+  EXPECT_EQ(tokens[0][2], 35);
+  EXPECT_EQ(s.length, 35);
+}
+function TestTokenizeManyTokens() {
+  var tokens = TokenizeForTest(" xXx =   yY09;\n alert();");
+  EXPECT_EQ(tokens.length, 12);
+  var token_num = 0;
+  EXPECT_EQ(tokens[token_num][0], WHITE_SPACE_TOKEN);
+  EXPECT_EQ(tokens[token_num][1], 0);
+  EXPECT_EQ(tokens[token_num][2], 1);
+  ++token_num;
+  EXPECT_EQ(tokens[token_num][0], IDENTIFIER_TOKEN);
+  EXPECT_EQ(tokens[token_num][1], 1);
+  EXPECT_EQ(tokens[token_num][2], 3);
+  ++token_num;
+  EXPECT_EQ(tokens[token_num][0], WHITE_SPACE_TOKEN);
+  EXPECT_EQ(tokens[token_num][1], 4);
+  EXPECT_EQ(tokens[token_num][2], 1);
+  ++token_num;
+  EXPECT_EQ(tokens[token_num][0], "=");
+  EXPECT_EQ(tokens[token_num][1], 5);
+  EXPECT_EQ(tokens[token_num][2], 1);
+  ++token_num;
+  EXPECT_EQ(tokens[token_num][0], WHITE_SPACE_TOKEN);
+  EXPECT_EQ(tokens[token_num][1], 6);
+  EXPECT_EQ(tokens[token_num][2], 3);
+  ++token_num;
+  EXPECT_EQ(tokens[token_num][0], IDENTIFIER_TOKEN);
+  EXPECT_EQ(tokens[token_num][1], 9);
+  EXPECT_EQ(tokens[token_num][2], 4);
+  ++token_num;
+  EXPECT_EQ(tokens[token_num][0], ";");
+  EXPECT_EQ(tokens[token_num][1], 13);
+  EXPECT_EQ(tokens[token_num][2], 1);
+  ++token_num;
+  EXPECT_EQ(tokens[token_num][0], WHITE_SPACE_TOKEN);
+  EXPECT_EQ(tokens[token_num][1], 14);
+  EXPECT_EQ(tokens[token_num][2], 2);
+  ++token_num;
+  EXPECT_EQ(tokens[token_num][0], IDENTIFIER_TOKEN);
+  EXPECT_EQ(tokens[token_num][1], 16);
+  EXPECT_EQ(tokens[token_num][2], 5);
+  ++token_num;
+  EXPECT_EQ(tokens[token_num][0], "(");
+  EXPECT_EQ(tokens[token_num][1], 21);
+  EXPECT_EQ(tokens[token_num][2], 1);
+  ++token_num;
+  EXPECT_EQ(tokens[token_num][0], ")");
+  EXPECT_EQ(tokens[token_num][1], 22);
+  EXPECT_EQ(tokens[token_num][2], 1);
+  ++token_num;
+  EXPECT_EQ(tokens[token_num][0], ";");
+  EXPECT_EQ(tokens[token_num][1], 23);
+  EXPECT_EQ(tokens[token_num][2], 1);
+  
+  tokens = TokenizeForTest("a = \"hello world/* sdf */\"; /* ha! \"ha!\" */");
+  EXPECT_EQ(tokens.length, 8);
+  var token_num = 0;
+  EXPECT_EQ(tokens[token_num][0], IDENTIFIER_TOKEN);
+  EXPECT_EQ(tokens[token_num][1], 0);
+  EXPECT_EQ(tokens[token_num][2], 1);
+  ++token_num;
+  EXPECT_EQ(tokens[token_num][0], WHITE_SPACE_TOKEN);
+  EXPECT_EQ(tokens[token_num][1], 1);
+  EXPECT_EQ(tokens[token_num][2], 1);
+  ++token_num;
+  EXPECT_EQ(tokens[token_num][0], '=');
+  EXPECT_EQ(tokens[token_num][1], 2);
+  EXPECT_EQ(tokens[token_num][2], 1);
+  ++token_num;
+  EXPECT_EQ(tokens[token_num][0], WHITE_SPACE_TOKEN);
+  EXPECT_EQ(tokens[token_num][1], 3);
+  EXPECT_EQ(tokens[token_num][2], 1);
+  ++token_num;
+  EXPECT_EQ(tokens[token_num][0], STRING_TOKEN);
+  EXPECT_EQ(tokens[token_num][1], 4);
+  EXPECT_EQ(tokens[token_num][2], 22);
+  ++token_num;
+  EXPECT_EQ(tokens[token_num][0], ";");
+  EXPECT_EQ(tokens[token_num][1], 26);
+  EXPECT_EQ(tokens[token_num][2], 1);
+  ++token_num;
+  EXPECT_EQ(tokens[token_num][0], WHITE_SPACE_TOKEN);
+  EXPECT_EQ(tokens[token_num][1], 27);
+  EXPECT_EQ(tokens[token_num][2], 1);
+  ++token_num;
+  EXPECT_EQ(tokens[token_num][0], COMMENT_TOKEN);
+  EXPECT_EQ(tokens[token_num][1], 28);
+  EXPECT_EQ(tokens[token_num][2], 15);
+}
+function TestTokenizeBadNumbers() {
+  var tokens = TokenizeForTest("xx999.0993");
+  EXPECT_EQ(tokens.length, 3);
+  var token_num = 0;
+  EXPECT_EQ(tokens[token_num][0], IDENTIFIER_TOKEN);
+  EXPECT_EQ(tokens[token_num][1], 0);
+  EXPECT_EQ(tokens[token_num][2], 5);
+  ++token_num;
+  EXPECT_EQ(tokens[token_num][0], ".");
+  EXPECT_EQ(tokens[token_num][1], 5);
+  EXPECT_EQ(tokens[token_num][2], 1);
+  ++token_num;
+  EXPECT_EQ(tokens[token_num][0], NUMBER_TOKEN);
+  EXPECT_EQ(tokens[token_num][1], 6);
+  EXPECT_EQ(tokens[token_num][2], 4);
+
+  tokens = TokenizeForTest("999xyz");
+  EXPECT_EQ(tokens.length, 2);
+  token_num = 0;
+  EXPECT_EQ(tokens[token_num][0], NUMBER_TOKEN);
+  EXPECT_EQ(tokens[token_num][1], 0);
+  EXPECT_EQ(tokens[token_num][2], 3);
+  ++token_num;
+  EXPECT_EQ(tokens[token_num][0], IDENTIFIER_TOKEN);
+  EXPECT_EQ(tokens[token_num][1], 3);
+  EXPECT_EQ(tokens[token_num][2], 3);
+
+  tokens = TokenizeForTest("999.xyz");
+  EXPECT_EQ(tokens.length, 2);
+  token_num = 0;
+  EXPECT_EQ(tokens[token_num][0], NUMBER_TOKEN);
+  EXPECT_EQ(tokens[token_num][1], 0);
+  EXPECT_EQ(tokens[token_num][2], 4);
+  ++token_num;
+  EXPECT_EQ(tokens[token_num][0], IDENTIFIER_TOKEN);
+  EXPECT_EQ(tokens[token_num][1], 4);
+  EXPECT_EQ(tokens[token_num][2], 3);
+
+  tokens = TokenizeForTest("999.345xyz");
+  EXPECT_EQ(tokens.length, 2);
+  token_num = 0;
+  EXPECT_EQ(tokens[token_num][0], NUMBER_TOKEN);
+  EXPECT_EQ(tokens[token_num][1], 0);
+  EXPECT_EQ(tokens[token_num][2], 7);
+  ++token_num;
+  EXPECT_EQ(tokens[token_num][0], IDENTIFIER_TOKEN);
+  EXPECT_EQ(tokens[token_num][1], 7);
+  EXPECT_EQ(tokens[token_num][2], 3);
+}
+function TestCleanTokens() {
+  var text = "  x /* hi */ = y 100.0 \" \" ;";
+  var tokens = TokenizeForTest(text)
+  EXPECT_EQ(tokens.length, 14);
+  tokens = CleanTokens(tokens, text);
+  EXPECT_EQ(tokens.length, 6);
+  var idx = 0;
+  EXPECT_EQ(tokens[idx][0], IDENTIFIER_TOKEN);
+  EXPECT_EQ(tokens[idx][1], 2);
+  EXPECT_EQ(tokens[idx][2], 1);
+  ++idx;
+  EXPECT_EQ(tokens[idx][0], "=");
+  EXPECT_EQ(tokens[idx][1], 13);
+  EXPECT_EQ(tokens[idx][2], 1);
+  ++idx;
+  EXPECT_EQ(tokens[idx][0], IDENTIFIER_TOKEN);
+  EXPECT_EQ(tokens[idx][1], 15);
+  EXPECT_EQ(tokens[idx][2], 1);
+  ++idx;
+  EXPECT_EQ(tokens[idx][0], NUMBER_TOKEN);
+  EXPECT_EQ(tokens[idx][1], 17);
+  EXPECT_EQ(tokens[idx][2], 5);
+  ++idx;
+  EXPECT_EQ(tokens[idx][0], STRING_TOKEN);
+  EXPECT_EQ(tokens[idx][1], 23);
+  EXPECT_EQ(tokens[idx][2], 3);
+  ++idx;
+  EXPECT_EQ(tokens[idx][0], ";");
+  EXPECT_EQ(tokens[idx][1], 27);
+  EXPECT_EQ(tokens[idx][2], 1);
+}
+function TestParseSimpleStatements() {
+  var model = Parse("x = \"hello world\";");
+  EXPECT_EQ(model.statement_list.length, 1);
+  EXPECT_EQ(model.statement_list[0].first_token_idx, 0);
+  EXPECT_EQ(model.statement_list[0].last_token_idx, 3);
+
+  model = Parse("x = 1; y = 2;");
+  EXPECT_EQ(model.statement_list.length, 2);
+  EXPECT_EQ(model.statement_list[0].first_token_idx, 0);
+  EXPECT_EQ(model.statement_list[0].last_token_idx, 3);
+  EXPECT_EQ(model.statement_list[1].first_token_idx, 4);
+  EXPECT_EQ(model.statement_list[1].last_token_idx, 7);
+
+  try {
+    model = Parse(" /* irrelevant */ x = \"needs a ;\"");
+    FAIL("Should throw an error.");
+  } catch(err) {
+    EXPECT_EQ(err,
+        "ERROR: Unterminated Statement.\nidx = 18\n" +
+        " /* irrelevant */ x/* Here */ = \"needs a ;\"");
+  }
+}
+function TestParseExpression() {
+  var model = Parse("x = 13;");
+  EXPECT_EQ(model.statement_list[0].name, "x");
+  expr = model.statement_list[0].expression;
+  EXPECT_EQ(ExpressionDebugString(model, expr), "13");
+  EXPECT_EQ(expr.type, NUMBER_TOKEN);
+  EXPECT_EQ(expr.right, 13);
+  EXPECT_EQ(model.token_idx, 4);
+  EXPECT_EQ(model.data["x"], 13);
+
+  model = Parse("x = 1 + 2 + 3;");
+  expr = model.statement_list[0].expression;
+  EXPECT_EQ(ExpressionDebugString(model, expr), "1 2 + 3 +");
+  EXPECT_EQ(expr.type, "+");
+  EXPECT_EQ(expr.right.type, NUMBER_TOKEN);
+  EXPECT_EQ(expr.right.right, 3);
+  expr = expr.left;
+  EXPECT_EQ(expr.type, "+");
+  EXPECT_EQ(expr.right.type, NUMBER_TOKEN);
+  EXPECT_EQ(expr.right.right, 2);
+  EXPECT_EQ(expr.left.type, NUMBER_TOKEN);
+  EXPECT_EQ(expr.left.right, 1);
+  EXPECT_EQ(model.token_idx, 8);
+  EXPECT_EQ(model.data["x"], 6);
+
+  model = Parse("x = 10 - 2 - 4;");
+  expr = model.statement_list[0].expression;
+  EXPECT_EQ(ExpressionDebugString(model, expr), "10 2 - 4 -");
+  EXPECT_EQ(expr.type, "-");
+  EXPECT_EQ(expr.right.type, NUMBER_TOKEN);
+  EXPECT_EQ(expr.right.right, 4);
+  expr = expr.left;
+  EXPECT_EQ(expr.type, "-");
+  EXPECT_EQ(expr.right.type, NUMBER_TOKEN);
+  EXPECT_EQ(expr.right.right, 2);
+  EXPECT_EQ(expr.left.type, NUMBER_TOKEN);
+  EXPECT_EQ(expr.left.right, 10);
+  EXPECT_EQ(model.token_idx, 8);
+  EXPECT_EQ(model.data["x"], 4);
+
+  model = Parse("x = 2 * 4 * 9;");
+  expr = model.statement_list[0].expression;
+  EXPECT_EQ(ExpressionDebugString(model, expr), "2 4 * 9 *");
+  EXPECT_EQ(model.data["x"], 72);
+
+  model = Parse("x = 64 / 4 / 2;");
+  expr = model.statement_list[0].expression;
+  EXPECT_EQ(ExpressionDebugString(model, expr), "64 4 / 2 /");
+  EXPECT_EQ(model.data["x"], 8);
+
+  model = Parse("x = 1 + 4 * 9 / 2 - 5;");
+  expr = model.statement_list[0].expression;
+  EXPECT_EQ(ExpressionDebugString(model, expr), "1 4 9 * 2 / + 5 -");
+  EXPECT_EQ(model.data["x"], 14);
+
+  model = Parse("x = (((6 - 2 -2)));");
+  expr = model.statement_list[0].expression;
+  EXPECT_EQ(ExpressionDebugString(model, expr),
+            "6 2 - 2 -");
+  EXPECT_EQ(model.data["x"], 2);
+
+  model = Parse("x = 1 + (1 + 3) * 3 / 2 / 2;");
+  expr = model.statement_list[0].expression;
+  EXPECT_EQ(ExpressionDebugString(model, expr),
+            "1 1 3 + 3 * 2 / 2 / +");
+  EXPECT_EQ(model.data["x"], 4);
+
+  model = Parse("x = (11 * 5 - 50);");
+  expr = model.statement_list[0].expression;
+  EXPECT_EQ(ExpressionDebugString(model, expr),
+            "11 5 * 50 -");
+  EXPECT_EQ(model.data["x"], 5);
+
+  model = Parse("x = (6 - 2 -2) * (1 + (1 + 3) * 3 / 2 / 2) - (11 * 5 - 50);");
+  expr = model.statement_list[0].expression;
+  EXPECT_EQ(ExpressionDebugString(model, expr),
+            "6 2 - 2 - 1 1 3 + 3 * 2 / 2 / + * 11 5 * 50 - -");
+  EXPECT_EQ(model.data["x"], 3);
+}
+function TestAll() {
+  TestTokenizeNumbers();
+  TestTokenizeOneToken();
+  TestTokenizeManyTokens();
+  TestTokenizeBadNumbers();
+  TestCleanTokens();
+  var o = {};
+  TestParseSimpleStatements();
+  TestParseExpression();
+  console.log("Test Complete.");
+}
+TestAll();
+
